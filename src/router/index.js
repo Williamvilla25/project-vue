8b2f82d0601ea0a2882b9171fc075970e6ac7f99
Williamvilla25 <williamvilla727@gmail.com>
2025-05-21 19:45:42 -0500

    Acomodando rutas edit add y new comuna

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,33 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
+import EditarComuna from '../components/comunas/EditarComuna,vue'
+import CrearComuna from '../components/comunas/NewrComuna.vue'
 import Comunas from '../views/Comunas.vue'
+import HomeView from '../views/HomeView.vue'
 
 
 const routes = [
   {
+   path: '/',
+   name: 'home',
+   component: Home
+  },
+  {
     path: '/comunas',
     name: 'Comunas',
     component: Comunas
+  },
+  {
+    path: '/editar-comuna/:id',
+    name: 'EditarComuna',
+    name: 'EditarComunas',
+    component: EditarComunas
+  },
+  {
+    path: '/add-comuna/:id',
+    name: 'NewComuna',
+    name: 'NewComunas',
+    component: NewComunas
   },
   {
     path: '/about',
